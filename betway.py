ddda4185e8ff3fb2fd5df1eed5bae158388b878f
a1nagrecha <68285757+a1nagrecha@users.noreply.github.com>
2020-07-20 20:08:46 +0100

    Add files via upload

--- betway.py
+++ betway.py
@@ -29,13 +29,13 @@ driver.implicitly_wait(10)
 #retrieving page source and parsing it
 data = driver.page_source
 soup = BeautifulSoup(data, "html.parser")
-#scrolls down the web page 
-
+#the following code which is commented is used if I want the odds from one or more specific events
 #driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 #ufc251 = driver.find_element_by_xpath("/html/body/div/div/div[3]/div/div[1]/div/div[2]/div[4]/div/div[2]/div/div[2]/div/div[2]/div[1]/div/div[1]/div[1]")
 #coordinates = ufc251.location_once_scrolled_into_view # returns dict of X, Y coordinates
 #driver.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
 #ufc251.click()
+#scrolls down the web page 
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 time.sleep(3)
 #produces an array for the odds
@@ -45,7 +45,6 @@ odd = driver.find_elements_by_class_name("odds")
 #for loop to put the odds into an array
 for i in range(0,len(odd)):
     oddt.append(odd[i].text)
-#print('oddt: ',oddt)
 #finds all elements with the names of each fighter
 f2 = driver.find_elements_by_css_selector('.teamNameFirstPart.teamNameAwayTextFirstPart')
 f1 = driver.find_elements_by_css_selector('.teamNameFirstPart.teamNameHomeTextFirstPart')
@@ -53,19 +52,11 @@ f1 = driver.find_elements_by_css_selector('.teamNameFirstPart.teamNameHomeTextFi
 #creates arrays for the fighters names 
 f2t = []
 f1t = []
+f3t = []
 #fills the arrays for the fighters nanes
 for i in range(0,len(f2)):
     f2t.append(f2[i].text)
     f1t.append(f1[i].text)
-#print('f1t: ', f1t)
-of1 = []
-of2 = []
-
-#for i in range(0,len(oddt),2):
- #   of1.append(oddt[i])
-  #  of2.append(oddt[i+1])
-    
-f3t = []
 
 for i in range(0,len(f2t)):
     f3t.append(f1t[i])
@@ -74,14 +65,5 @@ for i in range(0,len(f2t)):
 for i in range(0,len(oddt)-1):
     oddt[i] = oddt[i+1]    
     
-#for i in range(0,len(of2)):
- #   print(of2[i], f2t[i+3])
-df = pd.DataFrame(list(zip(f3t,oddt)), 
-               columns =['name','odd']) 
-
-pd.set_option("display.max_rows", None, "display.max_columns", None)
-
-
-#print(df)
     
-#driver.quit()
+driver.quit()
--- skybet.py
+++ skybet.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import re
 import time 
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-import json
-import requests 
-from lxml import html
 
 
 driver = webdriver.Chrome()
@@ -26,62 +17,48 @@ soup = BeautifulSoup(data, "html.parser")
 
 
 link = driver.find_elements_by_class_name("split__title")
-#close the cookie
+#close the cookie, as it is in the way of a clickable element
 driver.find_element_by_class_name("js-page__cookies-accept").click()
 #finding elements to click to find the correct page, and waiting for each page to load
 link = driver.find_elements_by_class_name("split__title")
+#declaring variable that waits for a maximum of five seconds
 wait = WebDriverWait(driver, 5)
-wait.until(lambda ignore:
-          driver.find_element_by_class_name("split__title").is_displayed()
-         and driver.find_element_by_class_name("split__title").is_enabled()
-        , "Element is clickable")
-#time.sleep(2)
-link[0].click()
-
-
-
+#link2 holds all the elements associated with each bout
 link2 = driver.find_elements_by_class_name("cell-text__line")
-link2[0].click()
-#producing lists that hold odds and the respective names
-odds_link = driver.find_elements_by_class_name("js-oc-price ")
-names_link = driver.find_elements_by_class_name("cell-text__line")
+#delcaring array that will hold odds values and the respective names
 names = []
 odds = []
-print('len ', len(link2))
-for i in range(0,2):
-   odds.append(odds_link[i].text)
-   names.append(names_link[i].text)    
-#returning to the original webpage
-driver.execute_script("window.history.go(-1)")
-for i in range(0, len(link2)-1):
+#for loop cycles through all the boats and adds odds and names to an array
+for i in range(0, len(link2) - 1):
     print(i)
-    time.sleep(1)
-    #wait.until(lambda ignore:
-     #   driver.find_element_by_class_name("split__title").is_displayed()
-      #  and driver.find_element_by_class_name("split__title").is_enabled()
-       # , "Element is clickable")
+    #waits for split title element to be clickable
+    wait.until(lambda ignore:
+        driver.find_element_by_class_name("split__title").is_displayed()
+        and driver.find_element_by_class_name("split__title").is_enabled()
+        , "Element is clickable")
     link = driver.find_elements_by_class_name("split__title")
-    for x in range(0,len(link)-1):
+    #for loop clicks on every link to allow access to each element associated with "cell-text__line" elements
+    for x in range(0,len(link)):
         coordinates = link[x].location_once_scrolled_into_view # returns dict of X, Y coordinates
         driver.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
         link[x].click()
-    time.sleep(1)
-    #wait.until(lambda ignore:
-     #          driver.find_element_by_class_name("cell-text__line").is_displayed()
-      #         and driver.find_element_by_class_name("cell-text__line").is_enabled()
-       #        , "Element is clickable")
+    #waits for element to be clickable
+    wait.until(lambda ignore:
+               driver.find_element_by_class_name("cell-text__line").is_displayed()
+               and driver.find_element_by_class_name("cell-text__line").is_enabled()
+               , "Element is clickable")
     link2 = driver.find_elements_by_class_name("cell-text__line")
+    #scrolls to the specific element, so it can be clicked
     coordinates = link2[i].location_once_scrolled_into_view # returns dict of X, Y coordinates
     driver.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
     link2[i].click()
-   # time.sleep(1)
 #producing lists that hold odds and the respective names
     odds1 = (driver.find_elements_by_class_name("js-oc-price "))
     names1 = (driver.find_elements_by_class_name("cell-text__line"))
     for n in range(0,len(names1)):
         odds.append(odds1[n].text)
         names.append(names1[n].text)
+    #goes back a web page so the start of the for loop can restart
     driver.execute_script("window.history.go(-1)")
-#print(names,' ',odds)
 
 driver.quit()
